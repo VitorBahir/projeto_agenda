f822568aa500810036a4d213bd0e82e90042f68c
Vitor Bahir de Andrade <105059969+VitorBahir@users.noreply.github.com>
2024-01-21 17:50:37 -0300

    adicionado mais campos no form e pt-BR

--- contact/models.py
+++ contact/models.py
@@ -13,23 +13,24 @@ class Category(models.Model):
 
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254, blank=True)
-    description = models.TextField(blank=True)
-    created_date = models.DateTimeField(default=timezone.now)
-    show = models.BooleanField(default=True) 
-    pictures = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    first_name = models.CharField(max_length=50, verbose_name='Nome')
+    last_name = models.CharField(max_length=50, verbose_name='Sobrenome')
+    phone = models.CharField(max_length=50, verbose_name='Telefone')
+    email = models.EmailField(max_length=254, blank=True, verbose_name='E-mail')
+    description = models.TextField(blank=True, verbose_name='Descrição')
+    created_date = models.DateTimeField(default=timezone.now, verbose_name='Data de criação')
+    show = models.BooleanField(default=True, verbose_name='Mostrar') 
+    pictures = models.ImageField(blank=True, upload_to='pictures/%Y/%m/', verbose_name='Fotos')
     category = models.ForeignKey(
         Category, 
         on_delete=models.SET_NULL,
         blank=True, null=True
-        )
+        , verbose_name='Categoria')
     owner = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
-        blank=True, null=True
+        blank=True, null=True,
+        verbose_name='Usuário'
     )
     
     def __str__(self) -> str:
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,28 +1,6 @@
-from django.db.models import Q
-from django.core.exceptions import ValidationError
-from django.shortcuts import get_object_or_404, redirect, render 
-from django import forms
+from django.shortcuts import render 
 
-from contact.models import Contact
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone',          
-        )
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        self.add_error(
-            None,
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-
-        return super().clean()
+from contact.forms import ContactForm
 
 def create(request):
     if request.method == 'POST':
