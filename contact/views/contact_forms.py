84803c1facbc9da96a0858627dcceabd42e7e120
Vitor Bahir de Andrade <105059969+VitorBahir@users.noreply.github.com>
2024-01-21 18:04:57 -0300

    redirecionando quando salvar contato

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,25 +1,16 @@
-from django.shortcuts import render 
-
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from contact.forms import ContactForm
 
 def create(request):
     if request.method == 'POST':
-        context = {
-        'form': ContactForm(request.POST)
-        }
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            new_contact = form.save()
+            return redirect('contact:contact', contact_id=new_contact.id)
 
-        return render(
-            request,
-            'contact/create.html',
-            context
-        )
+        context = {'form': form}
+        return render(request, 'contact/create.html', context)
 
-    context = {
-        'form': ContactForm()
-    }
-
-    return render(
-        request,
-        'contact/create.html',
-        context
-    )
+    context = {'form': ContactForm()}
+    return render(request, 'contact/create.html', context)
