3ddbe45bdfcae890846e21972a0229fae0b12dd0
Vitor Bahir de Andrade <105059969+VitorBahir@users.noreply.github.com>
2024-01-21 11:39:27 -0300

    Criado categoria, upload e download img admin

--- project/settings.py
+++ project/settings.py
@@ -106,11 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
-USE_I18N = True
+USE_I18N = False
 
 USE_TZ = True
 
@@ -124,6 +124,10 @@ STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
 
+STATIC_ROOT = BASE_DIR / 'static' #collectstatic
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
