0ee9a2639574d0e09912d1eb0ea4910012df95fe
Vitor Bahir de Andrade <105059969+VitorBahir@users.noreply.github.com>
2024-01-21 13:30:39 -0300

    criado tabela de contatos

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from contact.models import Contact
 
+
 def index(request):
     contacts = Contact.objects.all()
     context = {
-        'contacts' : contacts,
+        'contacts': contacts,
     }
     return render(
         request,
